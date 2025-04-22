182e4a4155ca6c5c303efadc20367d4480d097f0
Prabinthapadev21 <codinghub57@gmail.com>
2025-04-22 14:52:45 +0545

    Updated

---
+++ SimpleInteresst.php
@@ -0,0 +1,11 @@
+<!-- Program to calculate the simple interst -->
+<html>
+    <!-- sum for oue html form -->
+    <head><title>Simple Calculator</title>
+</head>
+    <body>
+        <?php
+        if(isset($_REQUEST['num1']))
+
+    </body>
+</html>
